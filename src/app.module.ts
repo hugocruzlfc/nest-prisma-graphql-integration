925235adb0ec715df6c6629d2445f955aaa8c281
Hugo Cruz <hugocruzlfc@gmail.com>
2025-03-26 16:26:37 +0100

    feat: add logger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 
 @Module({
   imports: [
+    CqrsModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
