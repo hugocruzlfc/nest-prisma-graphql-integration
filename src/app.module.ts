27d0da98d4b62cb08d9ccf2c243e15b24112cafc
Hugo Cruz <hugocruzlfc@gmail.com>
2025-03-26 15:24:47 +0100

    feat: add main config

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
+})
+export class AppModule {}
